83a8d56bceba231091fd4ae9acc083af2afb82b5
neip313 <neip313@naver.com>
2020-12-24 09:54:59 +0900

    'commit'

---
+++ 파이썬 알고리즘 인터뷰/ch12.graph/main.py
@@ -0,0 +1,10 @@
+from DFS import graph
+
+land = graph.Solution()
+
+
+
+if __name__ == '__main__':
+    print(land.numIslands(
+        [["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]]))
+
